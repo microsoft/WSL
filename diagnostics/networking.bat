64bcf39a3c309cb3361ac1ae5692ea2fd8995344
Blue <OneBlue@users.noreply.github.com>
2021-01-27 10:07:31 -0800

    Add more providers and packet capture to networking.bat (#6465)

--- diagnostics/networking.bat
+++ diagnostics/networking.bat
@@ -19,15 +19,23 @@ powershell.exe -NoProfile "Get-HnsNetwork | Where-Object {$_.Name -eq 'WSL'} | R
 :: Stop WSL
 net.exe stop LxssManager
 
+:: Start packet capture
+powershell.exe -NoProfile "New-NetEventSession HnsPacketCapture -CaptureMode SaveToFile -LocalFilePath %cd%\\packets.etl" || goto :fail
+powershell.exe -NoProfile "Add-NetEventPacketCaptureProvider -Level 5 -SessionName HnsPacketCapture -CaptureType BothPhysicalAndSwitch " || goto :fail
+powershell.exe -NoProfile "Start-NetEventSession HnsPacketCapture" || goto :fail
+
 :: Collect WSL logs
 wpr -start wsl_networking.wprp -filemode || goto :fail
 wsl.exe tr -d "\r" ^| bash < ./networking.sh
+
 wpr -stop wsl.etl || goto :fail
+powershell -NoProfile "Stop-NetEventSession HnsPacketCapture;  Remove-NetEventSession HnsPacketCapture" || goto :fail
 
 exit /b 0
 
 :fail
 echo Failed to collect WSL logs
+powershell -NoProfile "Stop-NetEventSession HnsPacketCapture; Remove-NetEventSession HnsPacketCapture"
 exit /b 1
 
 :admin
