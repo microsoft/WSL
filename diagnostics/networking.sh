b17df298eece84bb92003ba7f0b91ae4f6945ed5
Catalin-Emil Fetoiu <cfetoiu@microsoft.com>
2023-10-20 17:24:36 -0700

    check if proxy variables are set

--- diagnostics/networking.sh
+++ diagnostics/networking.sh
@@ -11,9 +11,15 @@ ip route show table all
 ip neighbor
 ip link
 
-# This will include the HTTP proxy env variables, if present
-echo "Printing all environment variables"
-printenv
+set +u
+[[ ! -v http_proxy ]] && echo "http_proxy is not set" || echo "http_proxy is set"
+[[ ! -v HTTP_PROXY ]] && echo "HTTP_PROXY is not set" || echo "HTTP_PROXY is set"
+[[ ! -v https_proxy ]] && echo "https_proxy is not set" || echo "https_proxy is set"
+[[ ! -v HTTPS_PROXY ]] && echo "HTTPS_PROXY is not set" || echo "HTTPS_PROXY is set"
+[[ ! -v no_proxy ]] && echo "no_proxy is not set" || echo "no_proxy is set"
+[[ ! -v NO_PROXY ]] && echo "NO_PROXY is not set" || echo "NO_PROXY is set"
+[[ ! -v WSL_PAC_URL ]] && echo "WSL_PAC_URL is not set" || echo "WSL_PAC_URL is set"
+set -u
 
 echo "Printing DNS configuration"
 cat /etc/resolv.conf
