a4bdb9c08f77ba0971a6a658a8b45d29f0a04ce5
Catalin-Emil Fetoiu <cfetoiu@microsoft.com>
2024-03-22 15:26:08 -0700

    add iptables/nftables

--- diagnostics/networking.sh
+++ diagnostics/networking.sh
@@ -22,3 +22,7 @@ if [ -z ${WSL_PAC_URL+x} ]; then echo "WSL_PAC_URL is unset"; else echo "WSL_PAC
 
 echo "Printing DNS configuration"
 cat /etc/resolv.conf
+
+echo "Printing iptables and nftables rules"
+iptables -S
+nft list ruleset
