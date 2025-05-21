ab10090df39560919c01cec9993e660e5ef91eef
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-20 21:21:08 -0400

    spelling: adapters
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/linux/unit_tests/vnet.c
+++ test/linux/unit_tests/vnet.c
@@ -139,7 +139,7 @@ static const LXT_VARIATION g_LxtVariations[] = {
     //{"Virtual Ethernet Pair namespace data check", VirtualEthernetPairNamespaceData},
 
     //
-    // Moving physical adpaters between namespaces is not currently supported
+    // Moving physical adapters between namespaces is not currently supported
     //
 
     //{"Physical device basic namespace check", PhysicalDeviceNamespace1},
