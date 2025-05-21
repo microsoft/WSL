92727bd8c3084befa1b22e354512c8f6fe761664
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-20 21:19:29 -0400

    spelling: red hat
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/windows/common/WslCoreConfig.cpp
+++ src/windows/common/WslCoreConfig.cpp
@@ -317,7 +317,7 @@ void wsl::core::Config::Initialize(_In_opt_ HANDLE UserToken)
     // Use the user-defined swap size if one was specified; otherwise, set to 25%
     // the memory size rounded up to the nearest GB.
     //
-    // N.B. This heuristic is modeled after RedHat and Ubuntu's recommended swap size.
+    // N.B. This heuristic is modeled after Red Hat and Ubuntu's recommended swap size.
     if (SwapSizeBytes == UINT64_MAX)
     {
         SwapSizeBytes = ((MemorySizeBytes / 4 + _1GB - 1) & ~(_1GB - 1));
