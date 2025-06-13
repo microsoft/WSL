957cc244c4ba496c705e6057e769317712293b1d
Blue <blue@bluecode.fr>
2025-06-12 22:16:29 -0700

    Update test distro

--- tools/test/build-test-distro.ps1
+++ tools/test/build-test-distro.ps1
@@ -40,13 +40,14 @@ echo "Building test_distro version: $version"
 Run { wsl.exe --install $Base --name test_distro --version 2 --no-launch }
 
 RunInDistro("apt update")
-RunInDistro("apt install daemonize libmount-dev genisoimage dosfstools make gcc socat systemd libpam-systemd dnsutils xz-utils bzip2 -f -y --no-install-recommends")
-RunInDistro("apt purge cpio isc-dhcp-client isc-dhcp-common nftables rsyslog vim vim-tiny vim-common whiptail xxd init genisoimage tasksel kmod bind9* -y -f --allow-remove-essential")
+RunInDistro("apt install daemonize libmount-dev genisoimage dosfstools make gcc socat systemd libpam-systemd bind9-dnsutils xz-utils bzip2 -f -y --no-install-recommends")
+RunInDistro("apt purge cpio isc-dhcp-client isc-dhcp-common nftables rsyslog vim vim-tiny vim-common whiptail xxd init genisoimage tasksel kmod mawk udev cron -y -f --allow-remove-essential")
 RunInDistro("apt-get autopurge -y")
 RunInDistro("apt clean")
 RunInDistro("umount /usr/lib/wsl/drivers")
 RunInDistro("umount /usr/lib/wsl/lib")
 RunInDistro("rm -rf /etc/wsl-distribution.conf /etc/wsl.conf /usr/share/doc/* /var/lib/apt/lists/* /var/log/* /var/cache/debconf/* /var/cache/ldconfig/* /usr/lib/wsl /usr/share/{gdb,vim,zsh,man}")
+RunInDistro('rm -rf -- $(ls /usr/share/locale ^| grep -vE "en|locale.alias")')
 RunInDistro("rm /usr/lib/systemd/user/{systemd-tmpfiles-setup.service,systemd-tmpfiles-clean.timer,systemd-tmpfiles-clean.service}")
 RunInDistro("rm /usr/lib/systemd/system/{systemd-tmpfiles-setup-dev.service,systemd-tmpfiles-setup.service,systemd-tmpfiles-clean.timer,systemd-tmpfiles-clean.service} /lib/systemd/system/{kmod-static-nodes.service,kmod.service,sysinit.target.wants/kmod-static-nodes.service}")
 Run { wsl.exe --export test_distro "test_distro.tar" }
