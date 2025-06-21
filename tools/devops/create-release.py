2ca0933ff17eb7de8090445863971cd110679bce
Blue <blue@bluecode.fr>
2025-06-20 18:05:07 -0700

    Use the github generated release notes by default in create-release.py

--- tools/devops/create-release.py
+++ tools/devops/create-release.py
@@ -18,7 +18,8 @@ from urllib.parse import urlparse
 @click.option('--no-fetch', is_flag=True, default=False)
 @click.option('--github-token', default=None)
 @click.option('--use-current-ref', is_flag=True, default=False)
-def main(version: str, previous: str, max_message_lines: int, publish: bool, assets: list, no_fetch: bool, github_token: str, use_current_ref: bool):
+@click.option('--auto-release-notes', is_flag=True, default=False)
+def main(version: str, previous: str, max_message_lines: int, publish: bool, assets: list, no_fetch: bool, github_token: str, use_current_ref: bool, auto_release_notes: bool):
     if publish:
         if assets is None:
             raise RuntimeError('--publish requires at least one asset')
@@ -37,30 +38,32 @@ def main(version: str, previous: str, max_message_lines: int, publish: bool, ass
     print(f'Creating release notes for: {previous} -> {current_ref}', file=sys.stderr)
 
     changes = ''
-    for e in get_change_list(None if use_current_ref else version, previous, not no_fetch):
 
-        # Detect attached github issues
-        issues = find_github_issues(e.message)
-        pr_description, pr_number = get_github_pr_message(github_token, e.message)
-        if pr_description is not None:
-            issues = issues.union(find_github_issues(pr_description))
+    if not auto_release_notes:
+        for e in get_change_list(None if use_current_ref else version, previous, not no_fetch):
 
-        message = e.message[:-1] if e.message.endswith('\n') else e.message
+            # Detect attached github issues
+            issues = find_github_issues(e.message)
+            pr_description, pr_number = get_github_pr_message(github_token, e.message)
+            if pr_description is not None:
+                issues = issues.union(find_github_issues(pr_description))
 
-        # Shrink the message if it's too long
-        lines = message.split('\n')
-        message = '\n'.join([e for e in lines if e][:max_message_lines])
+            message = e.message[:-1] if e.message.endswith('\n') else e.message
 
-        # Get rid of the github PR #
-        if pr_number is not None:
-            message = message.replace(f'(#{pr_number})', '')
+            # Shrink the message if it's too long
+            lines = message.split('\n')
+            message = '\n'.join([e for e in lines if e][:max_message_lines])
 
-        # Append to the changes (chr(92) == '\n')
-        message = f'{message.replace(chr(92), "")} (solves {",".join(issues)})' if issues else message
-        changes += f'* {message}\n'
+            # Get rid of the github PR #
+            if pr_number is not None:
+                message = message.replace(f'(#{pr_number})', '')
+
+            # Append to the changes (chr(92) == '\n')
+            message = f'{message.replace(chr(92), "")} (solves {",".join(issues)})' if issues else message
+            changes += f'* {message}\n'
 
     if publish:
-        publish_release(version, changes, assets, github_token)
+        publish_release(version, changes, assets, auto_release_notes, github_token)
     else:
         print(f'\n{changes}')
 
@@ -146,7 +149,7 @@ def get_change_list(version: str, previous: str, fetch: bool) -> list:
 
 
 @backoff.on_exception(backoff.expo, (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.RequestException), max_time=600)
-def publish_release(version: str, changes: str, assets: list, token: str):
+def publish_release(version: str, changes: str, assets: list, auto_release_notes: bool, token: str):
     print(f'Creating private Github release for: {version}', file=sys.stderr)
 
     # First create the release
@@ -157,10 +160,12 @@ def publish_release(version: str, changes: str, assets: list, token: str):
     content = {'tag_name': version,
                'target_commitish': 'master',
                'name': version,
-               'body': changes,
                "draft":True ,
                'prerelease':True ,
-               'generate_release_notes': False}
+               'generate_release_notes': auto_release_notes}
+
+    if changes:
+        content['body'] = changes
 
     response = requests.post('https://api.github.com/repos/microsoft/wsl/releases', json=content, headers=headers)
     response.raise_for_status()
