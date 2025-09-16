66f79a99c6f3f23b162bad3cc564239e985950ef
Blue <blue@bluecode.fr>
2025-09-16 15:54:52 -0700

    Increase the MSI installation timeout to 20 minutes

--- test/windows/InstallerTests.cpp
+++ test/windows/InstallerTests.cpp
@@ -249,7 +249,9 @@ class InstallerTests
 
         try
         {
-            wsl::shared::retry::RetryWithTimeout<void>(pred, std::chrono::seconds(1), std::chrono::minutes(2));
+            // It is possible for the 'DeprovisionMsix' stage of the MSI installation to take a long time.
+            // on vb_release, up to 8 minutes have been observed. Wait for up to 20 minutes to be safe.
+            wsl::shared::retry::RetryWithTimeout<void>(pred, std::chrono::seconds(1), std::chrono::minutes(20));
         }
         catch (...)
         {
