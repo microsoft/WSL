282fb1db97de4a7c487efbcfc27b3c5edc48da68
Blue <blue@bluecode.fr>
2025-10-21 16:08:13 -0700

    Save state

---
+++ src/windows/wslaservice/exe/resource.h
@@ -0,0 +1,15 @@
+/*++
+
+Copyright (c) Microsoft. All rights reserved.
+
+Module Name:
+
+    resource.h
+
+Abstract:
+
+    This file contains resource declarations for wslservice.exe
+
+--*/
+
+#define ID_ICON 1
