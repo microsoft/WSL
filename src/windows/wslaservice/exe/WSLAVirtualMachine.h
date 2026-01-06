da05ceaced766934fe0cbac35ed4fd745d1bd957
Ben Hillis <benhillis@gmail.com>
2026-01-06 11:55:50 -0800

    WSLA: Switch VHD boot disk approach and support PMEM for VHD disks (#13943)
    * WSLA: Switch to using PMEM for readonly VHDs that are added at boot
    
    * enum -> enum class
    
    * add support for SCSI and PMEM at boot
    
    * switch to std::variant
    
    * add tests for pmem vhds
    
    ---------
    
    Co-authored-by: Ben Hillis <benhill@ntdev.microsoft.com>

--- src/windows/wslaservice/exe/WSLAVirtualMachine.h
+++ src/windows/wslaservice/exe/WSLAVirtualMachine.h
@@ -106,7 +106,6 @@ private:
         const WSLA_PROCESS_FD* Fds, ULONG FdCount, const WSLA_PROCESS_FD** TtyInput, const WSLA_PROCESS_FD** TtyOutput, const WSLA_PROCESS_FD** TtyControl);
 
     void ConfigureNetworking();
-    void ConfigureMounts();
     void OnExit(_In_ const HCS_EVENT* Event);
     void OnCrash(_In_ const HCS_EVENT* Event);
     bool FeatureEnabled(WSLAFeatureFlags Flag) const;
@@ -117,6 +116,7 @@ private:
     int32_t ExpectClosedChannelOrError(wsl::shared::SocketChannel& Channel);
 
     ConnectedSocket ConnectSocket(wsl::shared::SocketChannel& Channel, int32_t Fd);
+    std::string GetVhdDevicePath(ULONG Lun);
     static void OpenLinuxFile(wsl::shared::SocketChannel& Channel, const char* Path, uint32_t Flags, int32_t Fd);
     void LaunchPortRelay();
     void RemoveShare(_In_ const MountedFolderInfo& MountInfo);
