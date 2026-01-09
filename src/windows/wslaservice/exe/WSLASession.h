3817a5e9188c33775ef2056ceac5a1db76e76db0
Blue <blue@bluecode.fr>
2026-01-09 10:13:14 -0800

    Format

--- src/windows/wslaservice/exe/WSLASession.h
+++ src/windows/wslaservice/exe/WSLASession.h
@@ -28,11 +28,9 @@ class WSLASession;
 class DECLSPEC_UUID("4559499B-4F07-4BD4-B098-9F4A432E9456") IWSLASessionImpl : public IInspectable
 {
 public:
-
     // N.B. The caller must maintain a reference to the COM object for the return pointer to be used safely.
     IFACEMETHOD(GetImplNoRef)(_Out_ WSLASession** Session) = 0;
 };
-
 
 class DECLSPEC_UUID("4877FEFC-4977-4929-A958-9F36AA1892A4") WSLASession
     : public Microsoft::WRL::RuntimeClass<Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::WinRtClassicComMix>, IWSLASession, IWSLASessionImpl, IFastRundown>
