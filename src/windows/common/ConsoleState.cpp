45baafab605a00152af3260b5b19829a4e494e74
Ben Hillis <benhill@ntdev.microsoft.com>
2026-01-21 16:01:25 -0800

    make opening CONIN and CONOUT non-fatal

--- src/windows/common/ConsoleState.cpp
+++ src/windows/common/ConsoleState.cpp
@@ -63,42 +63,53 @@ namespace wsl::windows::common {
 
 ConsoleState::ConsoleState()
 {
+    // Ensure console state is restored if the constructor throws.
+    auto cleanup = wil::scope_exit_log(WI_DIAGNOSTICS_INFO, [&]() { RestoreConsoleState(); });
+
     m_InputHandle.reset(
         CreateFileW(L"CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr));
 
-    THROW_LAST_ERROR_IF_MSG(!m_InputHandle, "CreateFileW(CONIN$) failed");
+    if (m_InputHandle)
+    {
+        m_SavedInputCodePage = GetConsoleCP();
+        LOG_IF_WIN32_BOOL_FALSE(SetConsoleCP(CP_UTF8));
+
+        // Configure for raw input with VT support.
+        DWORD mode;
+        THROW_LAST_ERROR_IF(!GetConsoleMode(m_InputHandle.get(), &mode));
+
+        DWORD NewMode = mode;
+        WI_SetAllFlags(NewMode, ENABLE_WINDOW_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT);
+        WI_ClearAllFlags(NewMode, ENABLE_ECHO_INPUT | ENABLE_INSERT_MODE | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
+        ChangeConsoleMode(m_InputHandle.get(), NewMode);
+        m_SavedInputMode = mode;
+    }
+    else
+    {
+        LOG_LAST_ERROR_MSG("CreateFileW(CONIN$) failed");
+    }
 
     m_OutputHandle.reset(
         CreateFileW(L"CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr));
 
-    THROW_LAST_ERROR_IF_MSG(!m_OutputHandle, "CreateFileW(CONOUT$) failed");
+    if (m_OutputHandle)
+    {
+        m_SavedOutputCodePage = GetConsoleOutputCP();
+        LOG_IF_WIN32_BOOL_FALSE(SetConsoleOutputCP(CP_UTF8));
 
-    // Ensure console state is restored if the constructor throws.
-    auto cleanup = wil::scope_exit_log(WI_DIAGNOSTICS_INFO, [&]() { RestoreConsoleState(); });
+        // Configure for VT output.
+        DWORD mode;
+        THROW_LAST_ERROR_IF(!GetConsoleMode(m_OutputHandle.get(), &mode));
 
-    // Set UTF-8 code page.
-    m_SavedInputCodePage = GetConsoleCP();
-    m_SavedOutputCodePage = GetConsoleOutputCP();
-    LOG_IF_WIN32_BOOL_FALSE(SetConsoleCP(CP_UTF8));
-    LOG_IF_WIN32_BOOL_FALSE(SetConsoleOutputCP(CP_UTF8));
-
-    // Configure for raw input with VT support.
-    DWORD mode;
-    THROW_LAST_ERROR_IF(!GetConsoleMode(m_InputHandle.get(), &mode));
-
-    DWORD NewMode = mode;
-    WI_SetAllFlags(NewMode, ENABLE_WINDOW_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT);
-    WI_ClearAllFlags(NewMode, ENABLE_ECHO_INPUT | ENABLE_INSERT_MODE | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
-    ChangeConsoleMode(m_InputHandle.get(), NewMode);
-    m_SavedInputMode = mode;
-
-    // Configure for VT output.
-    THROW_LAST_ERROR_IF(!GetConsoleMode(m_OutputHandle.get(), &mode));
-
-    NewMode = mode;
-    WI_SetAllFlags(NewMode, ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN);
-    ChangeConsoleMode(m_OutputHandle.get(), NewMode);
-    m_SavedOutputMode = mode;
+        DWORD NewMode = mode;
+        WI_SetAllFlags(NewMode, ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN);
+        ChangeConsoleMode(m_OutputHandle.get(), NewMode);
+        m_SavedOutputMode = mode;
+    }
+    else
+    {
+        LOG_LAST_ERROR_MSG("CreateFileW(CONOUT$) failed");
+    }
 
     cleanup.release();
 }
@@ -110,34 +121,47 @@ ConsoleState::~ConsoleState()
 
 void ConsoleState::RestoreConsoleState()
 {
-    if (m_SavedInputMode.has_value())
+    if (m_InputHandle)
     {
-        TrySetConsoleMode(m_InputHandle.get(), m_SavedInputMode.value());
+        if (m_SavedInputCodePage.has_value())
+        {
+            LOG_IF_WIN32_BOOL_FALSE(SetConsoleCP(m_SavedInputCodePage.value()));
+        }
+
+        if (m_SavedInputMode.has_value())
+        {
+            TrySetConsoleMode(m_InputHandle.get(), m_SavedInputMode.value());
+        }
     }
 
-    if (m_SavedOutputMode.has_value())
+    if (m_OutputHandle)
     {
-        TrySetConsoleMode(m_OutputHandle.get(), m_SavedOutputMode.value());
-    }
+        if (m_SavedOutputCodePage.has_value())
+        {
+            LOG_IF_WIN32_BOOL_FALSE(SetConsoleOutputCP(m_SavedOutputCodePage.value()));
+        }
 
-    if (m_SavedInputCodePage.has_value())
-    {
-        LOG_IF_WIN32_BOOL_FALSE(SetConsoleCP(m_SavedInputCodePage.value()));
-    }
-
-    if (m_SavedOutputCodePage.has_value())
-    {
-        LOG_IF_WIN32_BOOL_FALSE(SetConsoleOutputCP(m_SavedOutputCodePage.value()));
+        if (m_SavedOutputMode.has_value())
+        {
+            TrySetConsoleMode(m_OutputHandle.get(), m_SavedOutputMode.value());
+        }
     }
 }
 
 COORD ConsoleState::GetWindowSize() const
 {
-    CONSOLE_SCREEN_BUFFER_INFOEX Info{};
-    Info.cbSize = sizeof(Info);
-    THROW_IF_WIN32_BOOL_FALSE(GetConsoleScreenBufferInfoEx(m_OutputHandle.get(), &Info));
-    return {
-        static_cast<short>(Info.srWindow.Right - Info.srWindow.Left + 1), static_cast<short>(Info.srWindow.Bottom - Info.srWindow.Top + 1)};
+    if (m_OutputHandle)
+    {
+        CONSOLE_SCREEN_BUFFER_INFOEX Info{};
+        Info.cbSize = sizeof(Info);
+        THROW_IF_WIN32_BOOL_FALSE(GetConsoleScreenBufferInfoEx(m_OutputHandle.get(), &Info));
+        return {
+            static_cast<short>(Info.srWindow.Right - Info.srWindow.Left + 1),
+            static_cast<short>(Info.srWindow.Bottom - Info.srWindow.Top + 1)};
+    }
+
+    LOG_HR_MSG(E_UNEXPECTED, "No console handle available for GetWindowSize");
+    return {80, 24};
 }
 
 } // namespace wsl::windows::common
