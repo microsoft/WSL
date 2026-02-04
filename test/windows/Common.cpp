1aa1e065d630691cd583fb3e78223c1f265092fe
Blue <blue@bluecode.fr>
2026-02-03 16:32:50 -0800

    Force wait delay

--- test/windows/Common.cpp
+++ test/windows/Common.cpp
@@ -2040,9 +2040,7 @@ Return Value:
         LxsstuLaunchCommandAndCaptureOutput(cmd.data());
     }
 
-    auto wait = wil::scope_exit_log(WI_DIAGNOSTICS_INFO, [&]() {
-        std::this_thread::sleep_for(std::chrono::seconds(300));
-    });
+    auto wait = wil::scope_exit_log(WI_DIAGNOSTICS_INFO, [&]() { std::this_thread::sleep_for(std::chrono::seconds(300)); });
 
     WEX::TestExecution::RuntimeParameters::TryGetValue(L"LogDmesg", g_LogDmesgAfterEachTest);
 
