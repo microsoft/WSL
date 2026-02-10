f6695df026052d87199ec809b7ab370017192881
Richard Fricks <richfr@microsoft.com>
2026-02-10 12:25:59 -0800

    Fix clang formating issues. Make CPU/Mem optional. Formalize Struct and Enum names.

--- src/windows/WslcSDK/WslcsdkPrivate.h
+++ src/windows/WslcSDK/WslcsdkPrivate.h
@@ -16,9 +16,9 @@ Abstract:
 #include "wslcsdk.h"
 #include "wslaservice.h"
 #include <stdint.h>
-#include <wil/com.h>      // COM helpers
-//#include <wil/resource.h> // handle wrappers
-//#include <wil/result.h>   // error handling
+#include <wil/com.h> // COM helpers
+// #include <wil/resource.h> // handle wrappers
+// #include <wil/result.h>   // error handling
 
 // SESSION DEFINITIONS
 typedef struct WSLC_SESSION_OPTIONS_INTERNAL
@@ -30,14 +30,17 @@ typedef struct WSLC_SESSION_OPTIONS_INTERNAL
     uint64_t memoryMb;
     uint32_t timeoutMS;
 
-    WSLC_VHD_REQUIREMENTS vhdRequirements;
-    WSLC_SESSION_FLAGS flags;
+    WslcVhdRequirements vhdRequirements;
+    WslcSessionFlags flags;
     WslcSessionTerminationCallback terminationCallback;
     PVOID terminationCallbackContext;
 } WSLC_SESSION_OPTIONS_INTERNAL;
 
 static_assert(sizeof(WSLC_SESSION_OPTIONS_INTERNAL) == WSLC_SESSION_OPTIONS_SIZE, "WSLC_SESSION_OPTIONS_INTERNAL size mismatch");
-static_assert(__alignof(WSLC_SESSION_OPTIONS_INTERNAL) == WSLC_SESSION_OPTIONS_ALIGNMENT, "WSLC_SESSION_OPTIONS_INTERNAL alignment mismatch");
+
+static_assert(
+    __alignof(WSLC_SESSION_OPTIONS_INTERNAL) == WSLC_SESSION_OPTIONS_ALIGNMENT,
+    "WSLC_SESSION_OPTIONS_INTERNAL alignment mismatch");
 
 // PROCESS DEFINITIONS
 typedef struct WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL
@@ -50,9 +53,12 @@ typedef struct WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL
     PCSTR currentDirectory;
 } WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL;
 
-
-static_assert(sizeof(WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL) == WSLC_CONTAINER_PROCESS_OPTIONS_SIZE, "WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL must be 48 bytes");
-static_assert(__alignof(WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL) == WSLC_CONTAINER_PROCESS_OPTIONS_ALIGNMENT, "WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL must be 8-byte aligned");
+static_assert(
+    sizeof(WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL) == WSLC_CONTAINER_PROCESS_OPTIONS_SIZE,
+    "WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL must be 48 bytes");
+static_assert(
+    __alignof(WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL) == WSLC_CONTAINER_PROCESS_OPTIONS_ALIGNMENT,
+    "WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL must be 8-byte aligned");
 
 // CONTAINER DEFINITIONS
 typedef struct WSLC_CONTAINER_OPTIONS_INTERNAL
@@ -61,18 +67,20 @@ typedef struct WSLC_CONTAINER_OPTIONS_INTERNAL
     PCSTR runtimeName; // Container runtime name (expected to allow DNS resolution between containers)
     PCSTR HostName;
     PCSTR DomainName;
-    const WSLC_CONTAINER_PORT_MAPPING* ports;
+    const WslcContainerPortMapping* ports;
     UINT32 portsCount;
-    const WSLC_CONTAINER_VOLUME* volumes;
+    const WslcContainerVolume* volumes;
     UINT32 volumesCount;
     const WSLC_CONTAINER_PROCESS_OPTIONS_INTERNAL* initProcessOptions;
-    WSLC_ContainerNetworkingMode networking;
-    WSLC_CONTAINER_FLAGS containerFlags;
+    WslcContainerNetworkingMode networking;
+    WslcContainerFlags containerFlags;
 
 } WSLC_CONTAINER_OPTIONS_INTERNAL;
-
-static_assert(sizeof(WSLC_CONTAINER_OPTIONS_INTERNAL) == WSLC_CONTAINER_OPTIONS_SIZE, "WSLC_CONTAINER_OPTIONS_INTERNAL must be 80 bytes");
-static_assert(__alignof(WSLC_CONTAINER_OPTIONS_INTERNAL) == WSLC_CONTAINER_OPTIONS_ALIGNMENT,"WSLC_CONTAINER_OPTIONS_INTERNAL must be 8-byte aligned");
+static_assert(
+    sizeof(WSLC_CONTAINER_OPTIONS_INTERNAL) == WSLC_CONTAINER_OPTIONS_SIZE, "WSLC_CONTAINER_OPTIONS_INTERNAL must be 80 bytes");
+static_assert(
+    __alignof(WSLC_CONTAINER_OPTIONS_INTERNAL) == WSLC_CONTAINER_OPTIONS_ALIGNMENT,
+    "WSLC_CONTAINER_OPTIONS_INTERNAL must be 8-byte aligned");
 
 // Use to allocate the actual objects on the heap to keep it alive.
 struct WslcSessionImpl
@@ -89,4 +97,3 @@ struct WslcProcessImpl
 {
     wil::com_ptr<IWSLAProcess> process;
 };
-
