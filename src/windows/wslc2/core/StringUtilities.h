2ef8881a3ed15d2ac0b87343782940aa79f39a0a
David Bennett <dbenne@microsoft.com>
2026-02-12 03:50:44 -0800

    Remove string utils

--- src/windows/wslc2/core/StringUtilities.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-#pragma once
-#include "pch.h"
- 
-namespace wsl::windows::wslc::util
-{
-    std::wstring ToLower(std::wstring_view in);
-}
