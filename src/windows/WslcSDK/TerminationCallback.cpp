9ae45e33b54d08a2813c0c2acd5e17a067cd75a5
JohnMcPMS <johnmcp@microsoft.com>
2026-02-17 13:33:13 -0800

    Partial SDK implementation (#14211)
    Implements the functions called by the sample demo (as well as a few more trivial functions).  These are implemented as best as possible, but there are several TODO callouts for updates that are likely needed to the SDK API surface to account for disparities with the runtime API.  I intentionally did not make those changes to the SDK in this PR to limit the friction so that it might get merged sooner.

---
+++ src/windows/WslcSDK/TerminationCallback.cpp
@@ -0,0 +1,58 @@
+/*++
+
+Copyright (c) Microsoft. All rights reserved.
+
+Module Name:
+
+    TerminationCallback.cpp
+
+Abstract:
+
+    Implementation of a type that implements ITerminationCallback.
+
+--*/
+#include "precomp.h"
+#include "TerminationCallback.h"
+
+namespace {
+WslcSessionTerminationReason ConvertReason(WSLAVirtualMachineTerminationReason Reason)
+{
+    switch (Reason)
+    {
+    case WSLAVirtualMachineTerminationReasonShutdown:
+        return WSLC_SESSION_TERMINATION_REASON_SHUTDOWN;
+    case WSLAVirtualMachineTerminationReasonCrashed:
+        return WSLC_SESSION_TERMINATION_REASON_CRASHED;
+    default:
+        return WSLC_SESSION_TERMINATION_REASON_UNKNOWN;
+    }
+}
+} // namespace
+
+TerminationCallback::TerminationCallback(WslcSessionTerminationCallback callback, PVOID context) :
+    m_callback(callback), m_context(context)
+{
+}
+
+// TODO: Details from the runtime are dropped; should the SDK callback function be updated to include the reasons string?
+HRESULT STDMETHODCALLTYPE TerminationCallback::OnTermination(WSLAVirtualMachineTerminationReason Reason, LPCWSTR)
+{
+    if (m_callback)
+    {
+        m_callback(ConvertReason(Reason), m_context);
+    }
+
+    return S_OK;
+}
+
+winrt::com_ptr<TerminationCallback> TerminationCallback::CreateIf(const WSLC_SESSION_OPTIONS_INTERNAL* options)
+{
+    if (options->terminationCallback)
+    {
+        return winrt::make_self<TerminationCallback>(options->terminationCallback, options->terminationCallbackContext);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
