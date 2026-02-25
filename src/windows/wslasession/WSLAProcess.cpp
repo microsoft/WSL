bc76af5c9b2136dac32d65c97542120d3c538627
Ben Hillis <benhill@ntdev.microsoft.com>
2026-02-25 09:00:59 -0800

    fix flipped duplicatehandle arguments

--- src/windows/wslasession/WSLAProcess.cpp
+++ src/windows/wslasession/WSLAProcess.cpp
@@ -34,7 +34,7 @@ CATCH_RETURN();
 HRESULT WSLAProcess::GetExitEvent(HANDLE* Event)
 try
 {
-    *Event = wsl::windows::common::helpers::DuplicateHandle(m_control->GetExitEvent().get(), 0, FALSE, SYNCHRONIZE);
+    *Event = wsl::windows::common::helpers::DuplicateHandle(m_control->GetExitEvent().get(), SYNCHRONIZE, FALSE, 0);
     return S_OK;
 }
 CATCH_RETURN();
