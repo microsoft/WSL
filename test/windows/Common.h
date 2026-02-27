645e02c68f9fc9aa1f887bf1ada23836fa23bb9c
Ben Hillis <benhill@ntdev.microsoft.com>
2026-02-27 10:51:06 -0800

    wsla: fix test bugs
    - Add missing GetFileSizeEx in ExportContainer second block
    - Fix ContainerLabels duplicate-key false positive (empty vector, correct expected HRESULT)
    - Fix RegistryKeyChange move-assignment (return *this, transfer m_originalValue, self-assign guard)
    - Fix RegistryKeyChange::OpenKey to return actual key
    - Add missing return after content mismatch in ValidateProcessOutput
    - Fix MemoryMb 2024 -> 2048 typo in WSLATests and WslcSdkTests
    - Remove unused expectSignal parameter from ExpectCommandResult

--- test/windows/Common.h
+++ test/windows/Common.h
@@ -191,19 +191,25 @@ public:
 
     wil::unique_hkey OpenKey()
     {
-        return;
+        return wsl::windows::common::registry::CreateKey(m_hive, m_key, KEY_ALL_ACCESS);
     }
 
     RegistryKeyChange(const RegistryKeyChange&) = delete;
     RegistryKeyChange(RegistryKeyChange&& other) = default;
-    const RegistryKeyChange& operator=(RegistryKeyChange&& other)
+    RegistryKeyChange& operator=(RegistryKeyChange&& other)
     {
-        m_hive = std::move(other.m_hive);
-        m_key = std::move(other.m_key);
-        m_value = std::move(other.m_value);
+        if (this != &other)
+        {
+            m_hive = std::move(other.m_hive);
+            m_key = std::move(other.m_key);
+            m_value = std::move(other.m_value);
+            m_originalValue = std::move(other.m_originalValue);
 
-        other.m_hive = nullptr;
-        other.m_key = nullptr;
+            other.m_hive = nullptr;
+            other.m_key = nullptr;
+        }
+
+        return *this;
     }
 
     const RegistryKeyChange& operator=(RegistryKeyChange&) = delete;
