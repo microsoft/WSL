4882e88e8ac3adc8fd49b04528abafbfc2031936
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2026-03-03 11:05:08 -0800

    Clang format

--- test/windows/wslc/e2e/WSLCE2EContainerCreateTests.cpp
+++ test/windows/wslc/e2e/WSLCE2EContainerCreateTests.cpp
@@ -49,8 +49,8 @@ class WSLCE2EContainerCreateTests
         auto result = RunWslc(L"container create --name " + WslcContainerName + L" " + WslcInvalidImageNameAndTag);
         std::wstringstream expectedError;
         expectedError << L"Image '" << WslcInvalidImageNameAndTag << L"' not found, pulling\r\n"
-                      << L"pull access denied for library/" << WslcInvalidImageName
-                      << L", repository does not exist or may require 'docker login': denied: requested access to the resource is denied\r\n"
+                      << L"pull access denied for library/"
+                      << WslcInvalidImageName << L", repository does not exist or may require 'docker login': denied: requested access to the resource is denied\r\n"
                       << L"Error code: WSLA_E_IMAGE_NOT_FOUND\r\n";
         result.Verify({.Stderr = expectedError.str(), .ExitCode = 1});
     }
