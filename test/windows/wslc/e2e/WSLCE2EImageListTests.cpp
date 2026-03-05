6c6206a97b75e5c7929b6542bec04aa25b600699
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2026-03-05 13:09:51 -0800

    Added E2E test

---
+++ test/windows/wslc/e2e/WSLCE2EImageListTests.cpp
@@ -0,0 +1,74 @@
+/*++
+
+Copyright (c) Microsoft. All rights reserved.
+
+Module Name:
+
+    WSLCE2EImageListTests.cpp
+
+Abstract:
+
+    This file contains end-to-end tests for WSLC.
+--*/
+
+#include "precomp.h"
+#include "windows/Common.h"
+#include "WSLCExecutor.h"
+#include "WSLCE2EHelpers.h"
+
+namespace WSLCE2ETests {
+
+class WSLCE2EImageListTests
+{
+    WSL_TEST_CLASS(WSLCE2EImageListTests)
+
+    TEST_METHOD(WSLCE2E_Image_List_HelpCommand)
+    {
+        WSL2_TEST_ONLY();
+        auto result = RunWslc(L"image list --help");
+        result.Verify({.Stdout = GetHelpMessage(), .Stderr = L"", .ExitCode = S_OK});
+    }
+
+private:
+    const TestImage& DebianImage = DebianTestImage();
+
+    std::wstring GetHelpMessage() const
+    {
+        std::wstringstream output;
+        output << GetWslcHeader()              //
+               << GetDescription()             //
+               << GetUsage()                   //
+               << GetAvailableCommandAliases() //
+               << GetAvailableOptions();
+        return output.str();
+    }
+
+    std::wstring GetDescription() const
+    {
+        return L"Lists images.\r\n\r\n";
+    }
+
+    std::wstring GetUsage() const
+    {
+        return L"Usage: wslc image list [<options>]\r\n\r\n";
+    }
+
+    std::wstring GetAvailableCommandAliases() const
+    {
+        return L"The following command aliases are available: ls\r\n\r\n";
+    }
+
+    std::wstring GetAvailableOptions() const
+    {
+        std::wstringstream options;
+        options << L"The following options are available:\r\n"
+                << L"  --format      Output formatting (json or table) (Default:table)\r\n"
+                << L"  -q,--quiet    Outputs the container IDs only\r\n"
+                << L"  --session     Specify the session to use\r\n"
+                << L"  -v,--verbose  Output verbose details\r\n"
+                << L"  -h,--help     Shows help about the selected command\r\n"
+                << L"\r\n";
+        return options.str();
+    }
+};
+} // namespace WSLCE2ETests
