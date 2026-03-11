57d2481364350c9ff3d541b3d00df15b6da5e8d0
Blue <blue@bluecode.fr>
2026-03-10 18:25:52 -0700

    Add test coverage

--- test/windows/WSLATests.cpp
+++ test/windows/WSLATests.cpp
@@ -5328,6 +5328,7 @@ class WSLATests
             PruneResult result;
 
             VERIFY_ARE_EQUAL(m_defaultSession->PruneContainers(&filter, 1, 0, &result.result), E_POINTER);
+            VERIFY_ARE_EQUAL(m_defaultSession->PruneContainers(&filter, 1, 0, nullptr), E_POINTER);
         }
     }
 };
