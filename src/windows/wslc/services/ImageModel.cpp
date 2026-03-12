4bb3a6de132325916475a3ffd84b0839d7cf1952
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2026-03-12 14:28:05 -0700

    Clang format

--- src/windows/wslc/services/ImageModel.cpp
+++ src/windows/wslc/services/ImageModel.cpp
@@ -15,8 +15,7 @@ Abstract:
 #include "precomp.h"
 #include "ImageModel.h"
 
-namespace wsl::windows::wslc::models
-{
+namespace wsl::windows::wslc::models {
 RepoTag RepoTag::Parse(const std::string& repoTag)
 {
     auto lastSlashPos = repoTag.rfind('/');
@@ -27,22 +26,22 @@ RepoTag RepoTag::Parse(const std::string& repoTag)
         if (lastColonPos == std::string::npos)
         {
             // No slash and no colon (e.g. "debian")
-            return { repoTag, "" };
+            return {repoTag, ""};
         }
 
         // No slash but has colon (e.g. "debian:latest")
-        return { repoTag.substr(0, lastColonPos), repoTag.substr(lastColonPos + 1) };
+        return {repoTag.substr(0, lastColonPos), repoTag.substr(lastColonPos + 1)};
     }
 
     if (lastColonPos != std::string::npos && lastColonPos > lastSlashPos)
     {
         // Has slash and has colon after the last slash
         // (e.g. "myrepo/debian:latest")
-        return { repoTag.substr(0, lastColonPos), repoTag.substr(lastColonPos + 1) };
+        return {repoTag.substr(0, lastColonPos), repoTag.substr(lastColonPos + 1)};
     }
 
     // Has slash but no colon or colon before the last slash
     // (e.g. "myrepo/debian" or "myrepo:5000/debian")
-    return { repoTag, "" };
+    return {repoTag, ""};
 }
 } // namespace wsl::windows::wslc::models
