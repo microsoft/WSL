488ffb727a16e7e1fb552070c8eab5a3bfaf2666
John McPherson <johnmcp@microsoft.com>
2026-03-17 18:00:10 -0700

    format

--- src/windows/WslcSDK/IOCallback.cpp
+++ src/windows/WslcSDK/IOCallback.cpp
@@ -18,14 +18,13 @@ IOCallback::IOCallback(IWSLAProcess* process, const WslcContainerProcessIOCallba
 {
     using namespace wsl::windows::common::relay;
 
-    auto addIOCallback = [&](WslcProcessIOHandle ioHandle, WslcStdIOCallback callback, PVOID context)
-    {
+    auto addIOCallback = [&](WslcProcessIOHandle ioHandle, WslcStdIOCallback callback, PVOID context) {
         std::function<void(const gsl::span<char>& Buffer)> function;
         if (callback)
         {
             function = [callback, context](const gsl::span<char>& buffer) {
                 callback(reinterpret_cast<const BYTE*>(buffer.data()), static_cast<uint32_t>(buffer.size()), context);
-                };
+            };
         }
         else
         {
