68ebd0c4e76b9b71a64d2d2d7052c78cee60c12b
yao-msft <50888816+yao-msft@users.noreply.github.com>
2026-03-19 15:46:50 -0700

    cleanup

--- src/windows/wslc/services/SessionModel.cpp
+++ src/windows/wslc/services/SessionModel.cpp
@@ -19,26 +19,19 @@ Abstract:
 namespace wsl::windows::wslc::models {
 SessionOptions SessionOptions::Default()
 {
-    namespace s = wsl::windows::wslc::settings;
-
-    // StoragePath is PCWSTR; use a static to own the string for the process lifetime.
-    static const std::wstring storagePath = []() -> std::wstring {
-        auto configured = s::User().Get<s::Setting::SessionStoragePath>();
-        if (!configured.empty())
-        {
-            return configured;
-        }
-        return (wsl::windows::common::filesystem::GetLocalAppDataPath(nullptr) / L"wsla").wstring();
-    }();
+    // Use a function-local static to defer path initialization until first use.
+    static const std::filesystem::path defaultPath = {wsl::windows::common::filesystem::GetLocalAppDataPath(nullptr) / "wsla"};
 
     SessionOptions options{};
-    options.m_sessionSettings.DisplayName          = s_DefaultSessionName;
-    options.m_sessionSettings.CpuCount             = s::User().Get<s::Setting::SessionCpuCount>();
-    options.m_sessionSettings.MemoryMb             = s::User().Get<s::Setting::SessionMemoryMb>();
-    options.m_sessionSettings.BootTimeoutMs        = 30 * 1000;
-    options.m_sessionSettings.StoragePath          = storagePath.c_str();
-    options.m_sessionSettings.MaximumStorageSizeMb = s::User().Get<s::Setting::SessionStorageSizeMb>();
-    options.m_sessionSettings.NetworkingMode       = WSLANetworkingModeVirtioProxy;
+    options.m_sessionSettings.DisplayName = s_DefaultSessionName;
+    options.m_sessionSettings.CpuCount = settings::User().Get<settings::Setting::SessionCpuCount>();
+    options.m_sessionSettings.MemoryMb = settings::User().Get<settings::Setting::SessionMemoryMb>();
+    options.m_sessionSettings.BootTimeoutMs = 30 * 1000;
+    options.m_sessionSettings.StoragePath = settings::User().Get<settings::Setting::SessionStoragePath>().empty()
+                                                ? defaultPath.c_str()
+                                                : settings::User().Get<settings::Setting::SessionStoragePath>().c_str();
+    options.m_sessionSettings.MaximumStorageSizeMb = settings::User().Get<settings::Setting::SessionStorageSizeMb>();
+    options.m_sessionSettings.NetworkingMode = WSLANetworkingModeVirtioProxy;
     return options;
 }
 
