3cf4cfae27bedd7284265ea9d1f2b0c95c3a83c0
beena352 <beenachauhan@microsoft.com>
2026-04-02 20:50:08 -0700

    Allow small timestamp difference in ContainerRecoveryFromStorage test (#40081)
    * Allow small timestamp difference in ContainerRecoveryFromStorage test
    
    * feedback

--- test/windows/WSLCTests.cpp
+++ test/windows/WSLCTests.cpp
@@ -5238,7 +5238,12 @@ class WSLCTests
             wil::unique_cotaskmem_array_ptr<WSLCContainerPortMapping> ports;
             VERIFY_SUCCEEDED(session->ListContainers(&containers, containers.size_address<ULONG>(), &ports, ports.size_address<ULONG>()));
             VERIFY_ARE_EQUAL(containers.size(), 1);
-            VERIFY_ARE_EQUAL(containers[0].StateChangedAt, originalStateChangedAt);
+
+            // StateChangedAt may differ by ~1s between live (event time) and recovery (FinishedAt).
+            auto stateChangedAtDiff = (containers[0].StateChangedAt > originalStateChangedAt)
+                                          ? (containers[0].StateChangedAt - originalStateChangedAt)
+                                          : (originalStateChangedAt - containers[0].StateChangedAt);
+            VERIFY_IS_TRUE(stateChangedAtDiff <= 60);
             VERIFY_ARE_EQUAL(containers[0].CreatedAt, originalCreatedAt);
 
             VERIFY_SUCCEEDED(container.Get().Delete(WSLCDeleteFlagsNone));
