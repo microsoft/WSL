49b900611cb1522fc093cb2b067ca3f1dd2f0a79
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2026-04-03 09:33:47 -0700

    Init e2e test

--- test/windows/wslc/e2e/WSLCE2EContainerRunTests.cpp
+++ test/windows/wslc/e2e/WSLCE2EContainerRunTests.cpp
@@ -99,6 +99,74 @@ class WSLCE2EContainerRunTests
         VerifyContainerIsListed(WslcContainerName, L"running");
     }
 
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_NameRoot)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u root {} sh -c \"id -un; id -u; id -g\"",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stdout = L"root\n0\n0\n", .Stderr = L"", .ExitCode = 0});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_UidRoot)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u 0 {} id -u",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stdout = L"0\n", .Stderr = L"", .ExitCode = 0});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_UidGidRoot)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(L"container run --rm -u 0:0 {} sh -c \"id -u; id -g\"", DebianImage.NameAndTag()));
+        result.Verify({.Stdout = L"0\n0\n", .Stderr = L"", .ExitCode = 0});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_UnknownUser_Fails)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u user_does_not_exist {} id -u",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stderr = L"unable to find user user_does_not_exist: no matching entries in passwd file\r\nError code: E_FAIL\r\n", .ExitCode = 1});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_InvalidFormat_Fails)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u root:badgid {} id -u",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stderr = L"unable to find group badgid: no matching entries in group file\r\nError code: E_FAIL\r\n", .ExitCode = 1});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_NameGroupRoot)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u root:root {} sh -c \"id -un; id -u; id -g\"",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stdout = L"root\n0\n0\n", .Stderr = L"", .ExitCode = 0});
+    }
+
+    TEST_METHOD(WSLCE2E_Container_Run_UserOption_NonRootUser_Succeeds)
+    {
+        WSL2_TEST_ONLY();
+
+        auto result = RunWslc(std::format(
+            L"container run --rm -u nobody {} sh -c \"id -un; id -u; id -g\"",
+            DebianImage.NameAndTag()));
+        result.Verify({.Stdout = L"nobody\n65534\n65534\n", .Stderr = L"", .ExitCode = 0});
+    }
+
 private:
     const std::wstring WslcContainerName = L"wslc-test-container";
     const TestImage& DebianImage = DebianTestImage();
