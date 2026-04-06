9d39805c4d4db3cdc1af90100e0fe3a7734651c1
Blue <OneBlue@users.noreply.github.com>
2026-04-06 11:51:09 -0700

    Implement wslc session enter (#40088)
    * Save state
    
    * Save state
    
    * Save state
    
    * Cleanup
    
    * Prepare for PR
    
    * Cleanup
    
    * Validate flags
    
    * Apply PR suggestions
    
    * Add comment
    
    * Remove include
    
    * Cleanup diff
    
    * Format
    
    * Format
    
    * Restore session flags

--- src/windows/wslc/arguments/ArgumentDefinitions.h
+++ src/windows/wslc/arguments/ArgumentDefinitions.h
@@ -72,6 +72,7 @@ _(Remove,         "rm",                  NO_ALIAS,          Kind::Flag,        L
 /*_(Scheme,         "scheme",              NO_ALIAS,          Kind::Value,       Localization::WSLCCLI_SchemeArgDescription())*/ \
 _(Session,        "session",             NO_ALIAS,          Kind::Value,       Localization::WSLCCLI_SessionIdArgDescription()) \
 _(SessionId,      "session-id",          NO_ALIAS,          Kind::Positional,  Localization::WSLCCLI_SessionIdPositionalArgDescription()) \
+_(StoragePath,    "storage-path",        NO_ALIAS,          Kind::Positional,  L"Path to the session storage directory") \
 _(Signal,         "signal",              L"s",              Kind::Value,       Localization::WSLCCLI_SignalArgDescription(L"SIGKILL")) \
 _(Tag,            "tag",                 L"t",              Kind::Value,       Localization::WSLCCLI_TagArgDescription()) \
 _(Time,           "time",                L"t",              Kind::Value,       Localization::WSLCCLI_TimeArgDescription()) \
