018110cf1f5fb3037a0f701c3775f7bb546eefda
Feng Wang <wang6922@outlook.com>
2026-04-14 14:55:41 +0800

    cache socket information at bind

--- src/linux/bpf/bind_monitor.bpf.c
+++ src/linux/bpf/bind_monitor.bpf.c
@@ -12,76 +12,81 @@ struct {
     __uint(max_entries, BIND_MONITOR_RINGBUF_SIZE);
 } events SEC(".maps");
 
-// Track sockets that went through bind() so we only emit matching releases.
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
-    __type(key, __u64);    // sock pointer as key
-    __type(value, __u8);
+    __type(key, __u64);               // sock pointer as key
+    __type(value, struct bind_event); // snapshot from bind time
     __uint(max_entries, 65536);
 } bound_sockets SEC(".maps");
 
-static __always_inline int emit_event(struct sock *sk, __u8 is_bind, int ret)
+static __always_inline int emit_bind_event(struct sock *sk, int ret)
 {
     struct bind_event *e;
-    __u16 family;
-    __u16 protocol;
+    struct bind_event info = {};
     __u64 sk_key = (__u64)sk;
 
-    if (!sk)
+    if (!sk || ret != 0)
         return 0;
 
-    family = BPF_CORE_READ(sk, __sk_common.skc_family);
-    if (family != AF_INET && family != AF_INET6)
+    info.family = BPF_CORE_READ(sk, __sk_common.skc_family);
+    if (info.family != AF_INET && info.family != AF_INET6)
         return 0;
 
-    protocol = BPF_CORE_READ(sk, sk_protocol);
-    if (protocol != IPPROTO_TCP && protocol != IPPROTO_UDP)
+    info.protocol = BPF_CORE_READ(sk, sk_protocol);
+    if (info.protocol != IPPROTO_TCP && info.protocol != IPPROTO_UDP)
         return 0;
 
-    if (is_bind)
+    info.port = BPF_CORE_READ(sk, __sk_common.skc_num);
+    info.is_bind = 1;
+
+    if (info.family == AF_INET)
     {
-        // Only emit successful binds.
-        if (ret != 0)
-            return 0;
-
-        __u8 val = 1;
-        bpf_map_update_elem(&bound_sockets, &sk_key, &val, BPF_ANY);
+        struct inet_sock *inet = (struct inet_sock *)sk;
+        info.addr4 = BPF_CORE_READ(inet, inet_saddr);
     }
     else
     {
-        // Only emit release if this socket was previously bound.
-        if (!bpf_map_lookup_elem(&bound_sockets, &sk_key))
-            return 0;
-        bpf_map_delete_elem(&bound_sockets, &sk_key);
+        struct ipv6_pinfo *pinet6;
+        pinet6 = BPF_CORE_READ((struct inet_sock *)sk, pinet6);
+        if (pinet6)
+            BPF_CORE_READ_INTO(info.addr6, pinet6, saddr.in6_u.u6_addr8);
     }
+
+    bpf_map_update_elem(&bound_sockets, &sk_key, &info, BPF_ANY);
 
     e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
     if (!e)
         return 0;
 
-    e->family = family;
-    e->protocol = protocol;
-    e->port = BPF_CORE_READ(sk, __sk_common.skc_num);
-    e->is_bind = is_bind;
-    e->pad = 0;
+    *e = info;
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
 
-    if (family == AF_INET)
+static __always_inline int emit_release_event(struct sock *sk)
+{
+    struct bind_event *e;
+    struct bind_event *info;
+    __u64 sk_key = (__u64)sk;
+
+    if (!sk)
+        return 0;
+
+    info = bpf_map_lookup_elem(&bound_sockets, &sk_key);
+    if (!info)
+        return 0;
+
+    e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e)
     {
-        struct inet_sock *inet = (struct inet_sock *)sk;
-        e->addr4 = BPF_CORE_READ(inet, inet_saddr);
-        __builtin_memset(e->addr6, 0, sizeof(e->addr6));
-    }
-    else
-    {
-        struct ipv6_pinfo *pinet6;
-        e->addr4 = 0;
-        pinet6 = BPF_CORE_READ((struct inet_sock *)sk, pinet6);
-        if (pinet6)
-            BPF_CORE_READ_INTO(e->addr6, pinet6, saddr.in6_u.u6_addr8);
-        else
-            __builtin_memset(e->addr6, 0, sizeof(e->addr6));
+        bpf_map_delete_elem(&bound_sockets, &sk_key);
+        return 0;
     }
 
+    *e = *info;
+    e->is_bind = 0;
+
+    bpf_map_delete_elem(&bound_sockets, &sk_key);
     bpf_ringbuf_submit(e, 0);
     return 0;
 }
@@ -93,7 +98,7 @@ int BPF_PROG(fexit_inet_bind, struct socket *sock, struct sockaddr *uaddr,
              int addr_len, int ret)
 {
     struct sock *sk = BPF_CORE_READ(sock, sk);
-    return emit_event(sk, 1, ret);
+    return emit_bind_event(sk, ret);
 }
 
 // fexit/inet6_bind: called after inet6_bind() returns.
@@ -103,7 +108,7 @@ int BPF_PROG(fexit_inet6_bind, struct socket *sock, struct sockaddr *uaddr,
              int addr_len, int ret)
 {
     struct sock *sk = BPF_CORE_READ(sock, sk);
-    return emit_event(sk, 1, ret);
+    return emit_bind_event(sk, ret);
 }
 
 // fentry/inet_release: called when a socket is being closed.
@@ -112,7 +117,7 @@ SEC("fentry/inet_release")
 int BPF_PROG(fentry_inet_release, struct socket *sock)
 {
     struct sock *sk = BPF_CORE_READ(sock, sk);
-    return emit_event(sk, 0, 0);
+    return emit_release_event(sk);
 }
 
 char LICENSE[] SEC("license") = "GPL";
