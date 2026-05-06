fe638d08f7b062dfb4a77d53c57c97afa7ccbd80
Brian Perkins <bperkins@ntdev.microsoft.com>
2026-05-06 13:57:04 -0700

    Downgrade system IL to high

--- src/windows/common/WslSecurity.cpp
+++ src/windows/common/WslSecurity.cpp
@@ -172,7 +172,31 @@ wil::unique_handle wsl::windows::common::security::GetUserToken(_In_ TOKEN_TYPE 
 
     wil::unique_handle newToken;
     THROW_IF_WIN32_BOOL_FALSE(::DuplicateTokenEx(
-        contextToken.get(), TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, nullptr, SecurityImpersonation, tokenType, &newToken));
+        contextToken.get(),
+        TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_DEFAULT,
+        nullptr,
+        SecurityImpersonation,
+        tokenType,
+        &newToken));
+
+    // If the token integrity level is above high, reduce it to high integrity level. The VM worker process runs at
+    // high integrity level and objects created with a higher integrity level token may be inaccessible.
+    if (GetUserBasicIntegrityLevel(newToken.get()) == SECURITY_MANDATORY_SYSTEM_RID)
+    {
+        union
+        {
+            SID sid;
+            BYTE buffer[SECURITY_SID_SIZE(1)];
+        } sidBuffer;
+        SID_IDENTIFIER_AUTHORITY systemSidAuthority = SECURITY_MANDATORY_LABEL_AUTHORITY;
+        THROW_IF_NTSTATUS_FAILED(::RtlInitializeSidEx(&sidBuffer.sid, &systemSidAuthority, 1, SECURITY_MANDATORY_HIGH_RID));
+
+        TOKEN_MANDATORY_LABEL tokenLabel{};
+        tokenLabel.Label.Attributes = SE_GROUP_INTEGRITY;
+        tokenLabel.Label.Sid = &sidBuffer.sid;
+        THROW_IF_WIN32_BOOL_FALSE(::SetTokenInformation(
+            newToken.get(), TokenIntegrityLevel, &tokenLabel, (sizeof(tokenLabel) + ::GetLengthSid(&sidBuffer.sid))));
+    }
 
     return newToken;
 }
