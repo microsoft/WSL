b0e07003049654654ec176d2ec8067281ba9dee8
Feng Wang <wang6922@outlook.com>
2026-06-15 17:30:34 +0800

    Fix potential uint16_t counter overflow to 0

--- src/windows/wslcsession/WSLCVirtualMachine.cpp
+++ src/windows/wslcsession/WSLCVirtualMachine.cpp
@@ -1210,12 +1210,13 @@ std::shared_ptr<VmPortAllocation> WSLCVirtualMachine::AllocatePort(int Family, i
 {
     std::lock_guard lock{m_lock};
 
-    for (auto i = CONTAINER_PORT_RANGE.first; i <= CONTAINER_PORT_RANGE.second; i++)
+    for (uint32_t i = CONTAINER_PORT_RANGE.first; i <= CONTAINER_PORT_RANGE.second; i++)
     {
-        if (!m_allocatedPorts.contains(i))
+        uint16_t port = static_cast<uint16_t>(i);
+        if (!m_allocatedPorts.contains(port))
         {
-            WI_VERIFY(m_allocatedPorts.insert(i).second);
-            return std::make_shared<VmPortAllocation>(i, Family, Protocol, *this);
+            WI_VERIFY(m_allocatedPorts.insert(port).second);
+            return std::make_shared<VmPortAllocation>(port, Family, Protocol, *this);
         }
     }
 
