a0fc90de451ac1caafab9c3096e8678349219e8f
Blue <blue@bluecode.fr>
2026-07-08 17:46:46 -0700

    Pre-download socat to avoid test failings due to connectivity issues

--- test/windows/Common.h
+++ test/windows/Common.h
@@ -651,6 +651,8 @@ void VerifyPatternMatch(const std::string& Content, const std::string& Pattern);
 
 std::filesystem::path GetTestImagePath(std::string_view imageName);
 
+std::filesystem::path GetTestDataPath();
+
 void LoadTestImage(IWSLCSession& session, std::string_view imageName);
 
 void ExpectHttpResponse(LPCWSTR Url, std::optional<int> expectedCode, bool retry = false);
