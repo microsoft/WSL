7eb3ced67712074e425786d61c5b213737fd179c
Blue <OneBlue@users.noreply.github.com>
2026-08-12 21:28:53 +0000

    Mount plugin folders on behalf of the user owning the wsl session (#41331)

--- src/windows/service/exe/WslCoreVm.cpp
+++ src/windows/service/exe/WslCoreVm.cpp
@@ -2055,7 +2055,8 @@ void WslCoreVm::MountRootNamespaceFolder(_In_ LPCWSTR HostPath, _In_ LPCWSTR Gue
     auto lock = m_lock.lock_exclusive();
 
     const auto flags = (ReadOnly ? hcs::Plan9ShareFlags::ReadOnly : hcs::Plan9ShareFlags::None) | hcs::Plan9ShareFlags::AllowOptions;
-    wsl::windows::common::hcs::AddPlan9Share(m_system.get(), Name, Name, HostPath, LX_INIT_UTILITY_VM_PLAN9_PORT, flags);
+    wsl::windows::common::hcs::AddPlan9Share(
+        m_system.get(), Name, Name, HostPath, LX_INIT_UTILITY_VM_PLAN9_PORT, flags, m_userToken.get());
 
     wsl::shared::MessageWriter<LX_MINI_INIT_MOUNT_FOLDER_MESSAGE> message(LxMiniInitMountFolder);
     message.WriteString(message->PathIndex, GuestPath);
--- test/windows/PluginTests.h
+++ test/windows/PluginTests.h
@@ -37,11 +37,20 @@ enum class PluginTestType
     InitPidIsDifferent,
     FailToRegisterUnregisterDistro,
     RunDistroCommand,
-    GetUsername
+    GetUsername,
+    WslcSuccess,
+    WslcSessionRejected,
+    WslcContainerRejected,
+    WslcImagePull,
+    WslcVmRestart,
+    WslcVmStopCommitted,
+    WslcVmNeverStarted,
+    MountFolderAccess
 };
 
 constexpr auto c_testType = L"TestType";
 constexpr auto c_logFile = L"LogFile";
+constexpr auto c_mountFolder = L"MountFolder";
 
 inline wil::unique_hkey OpenTestRegistryKey(REGSAM AccessMask)
 {
