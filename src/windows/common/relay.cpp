31ab1cf0f6ff3ae347233a483ada0f4edbb5d712
Eamon <eamon112009@gmail.com>
2026-08-12 23:45:08 +0530

    Fix BidirectionalRelay half-close handling

--- src/windows/common/relay.cpp
+++ src/windows/common/relay.cpp
@@ -297,6 +297,8 @@ void wsl::windows::common::relay::BidirectionalRelay(_In_ HANDLE LeftHandle, _In
 
     bool leftReadPending = false;
     bool rightReadPending = false;
+    bool leftReadClosed = false;
+    bool rightReadClosed = false;
     auto cancelReads = wil::scope_exit_log(WI_DIAGNOSTICS_INFO, [&] {
         DWORD bytes;
         if (leftReadPending)
@@ -316,13 +318,13 @@ void wsl::windows::common::relay::BidirectionalRelay(_In_ HANDLE LeftHandle, _In
     const HANDLE waitObjects[] = {leftOverlapped.hEvent, rightOverlapped.hEvent};
     for (;;)
     {
-        if ((LeftHandle == nullptr) && (RightHandle == nullptr))
+        if (leftReadClosed && rightReadClosed)
         {
             break;
         }
 
         DWORD leftBytesRead = 0;
-        if (!leftReadPending && LeftHandle)
+        if (!leftReadPending && !leftReadClosed && LeftHandle)
         {
             if (!ReadFile(LeftHandle, leftReadSpan.data(), gsl::narrow_cast<DWORD>(leftReadSpan.size()), &leftBytesRead, &leftOverlapped))
             {
@@ -333,7 +335,7 @@ void wsl::windows::common::relay::BidirectionalRelay(_In_ HANDLE LeftHandle, _In
         }
 
         DWORD rightBytesRead = 0;
-        if (!rightReadPending && RightHandle)
+        if (!rightReadPending && !rightReadClosed && RightHandle)
         {
             if (!ReadFile(RightHandle, rightReadSpan.data(), gsl::narrow_cast<DWORD>(rightReadSpan.size()), &rightBytesRead, &rightOverlapped))
             {
@@ -352,7 +354,7 @@ void wsl::windows::common::relay::BidirectionalRelay(_In_ HANDLE LeftHandle, _In
             leftReadPending = false;
             if (leftBytesRead == 0)
             {
-                LeftHandle = nullptr;
+                leftReadClosed = true;
                 if (WI_IsFlagSet(Flags, RelayFlags::RightIsSocket))
                 {
                     LOG_LAST_ERROR_IF(shutdown(reinterpret_cast<SOCKET>(RightHandle), SD_SEND) == SOCKET_ERROR);
@@ -380,7 +382,7 @@ void wsl::windows::common::relay::BidirectionalRelay(_In_ HANDLE LeftHandle, _In
             rightReadPending = false;
             if (rightBytesRead == 0)
             {
-                RightHandle = nullptr;
+                rightReadClosed = true;
                 if (WI_IsFlagSet(Flags, RelayFlags::LeftIsSocket))
                 {
                     LOG_LAST_ERROR_IF(shutdown(reinterpret_cast<SOCKET>(LeftHandle), SD_SEND) == SOCKET_ERROR);
@@ -584,12 +586,6 @@ try
                 DWORD BytesRead{};
                 if (ReadFile(e.Handle, e.Buffer.data(), static_cast<DWORD>(e.Buffer.size()), &BytesRead, &e.Overlapped))
                 {
-                    if (BytesRead == 0)
-                    {
-                        e.State = Eof;
-                        continue;
-                    }
-
                     // IO is available.
                     Write(i, gsl::make_span(e.Buffer.data(), BytesRead));
 
