10a179a71dbb4fe468e72627de9b7038292deeef
Feng Wang <wang6922@outlook.com>
2026-08-17 16:33:38 +0800

    treat EFSBADCRC as disk corruption

--- src/windows/service/exe/WslCoreInstance.cpp
+++ src/windows/service/exe/WslCoreInstance.cpp
@@ -65,8 +65,8 @@ WslCoreInstance::WslCoreInstance(
 
     if (result.Result != 0)
     {
-        // N.B. EUCLEAN (117) can be returned if the disk's journal is corrupted.
-        if ((result.Result == EINVAL || result.Result == 117) && result.FailureStep == LxInitCreateInstanceStepMountDisk)
+        // N.B. EFSBADCRC (74) or EFSCORRUPTED (117) can be returned if the disk's journal is corrupted.
+        if ((result.Result == EINVAL || result.Result == 74 || result.Result == 117) && result.FailureStep == LxInitCreateInstanceStepMountDisk)
         {
             THROW_HR(WSL_E_DISK_CORRUPTED);
         }
