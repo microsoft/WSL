f4adb223373c284a427637bc7c8584080bfa9ea9
Feng Wang <wang6922@outlook.com>
2026-09-16 13:26:21 +0800

    move installer log collection after repro

--- diagnostics/collect-wsl-logs.ps1
+++ diagnostics/collect-wsl-logs.ps1
@@ -196,12 +196,6 @@ if (Test-Path $wslconfig)
     Copy-Item $wslconfig $folder | Out-Null
 }
 
-# Collect high-level WSL install log (written by WriteInstallLog() in install.cpp)
-Copy-Item "C:\Windows\temp\wsl-install-log.txt" $folder -ErrorAction ignore
-
-# Collect MSI verbose install log (preserved on failure by wsl --update or WslInstaller service).
-Copy-Item "$env:TEMP\wsl-install-logs.txt" $folder -ErrorAction ignore
-
 get-appxpackage MicrosoftCorporationII.WindowsSubsystemforLinux -ErrorAction Ignore > $folder/appxpackage.txt
 get-acl "C:\ProgramData\Microsoft\Windows\WindowsApps" -ErrorAction Ignore | Format-List > $folder/acl.txt
 Get-WindowsOptionalFeature -Online > $folder/optional-components.txt
@@ -438,6 +432,12 @@ if ($Dump)
         }
     }
 }
+
+# Collect high-level WSL install log (written by WriteInstallLog() in install.cpp)
+Copy-Item "C:\Windows\temp\wsl-install-log.txt" $folder -ErrorAction ignore
+
+# Collect MSI verbose install log (preserved on failure by wsl --update or WslInstaller service).
+Copy-Item "$env:TEMP\wsl-install-logs.txt" $folder -ErrorAction ignore
 
 $logArchive = "$(Resolve-Path $folder).tar.gz"
 tar.exe -czf $logArchive $folder
